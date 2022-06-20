a8d818f167d1157cea950d77976f72c1143978ab
ttjjmm <tjm468@163.com>
2022-06-20 19:08:38 +0800

    add mobilevitv2

---
+++ models/mobilevitv2.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+
+
+
+class MobileViTBlockv2(nn.Module):
+    def __init__(self):
+        super(MobileViTBlockv2, self).__init__()
+
+
+
+    def forward(self, x):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
